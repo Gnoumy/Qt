6143513c4bbf3bca5fc238f17fd8feccb08cbf63
gnoumy <gaucher.ju@gmail.com>
2018-09-26 14:10:01 +0200

    Exercice: lister les fichiers avec une RegEx

---
+++ regEx/main.cpp
@@ -0,0 +1,20 @@
+#include <QCoreApplication>
+#include <QDir>
+#include <QString>
+#include <QDebug>
+#include <QRegExp>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QDir dir = dir.homePath();;
+    QStringList listeFichier = dir.entryList();
+
+    QRegExp exp("\\.[\\w]{3}");
+    foreach ( QString l, listeFichier){
+        if ( l.contains(exp) == true)
+            qDebug() << l;
+    }
+
+    return a.exec();
+}
